08f487da168bd8e61ccc982a18197ce6028a2876
cutiebara <gronliee@gmail.com>
2024-02-22 08:44:37 +0100

    Added dvbook.no and a script to replace the default adress with the one it should have

---
+++ dvbook.no/calendar.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Google Calendar Location Replacer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Replaces "Location" with custom address in Google Calendar events
+// @author       cutiebara
+// @match        https://calendar.google.com/calendar/u/0/r/eventedit*
+// @icon         https://www.google.com/s2/favicons?domain=google.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.get("location") === "Location") {
+    urlParams.set("location", "Økern Torgvei 13, 0580 Oslo"); // Replace with your encoded coordinates
+    window.history.replaceState({}, "", `${location.pathname}?${urlParams}`);
+  }
+})();
